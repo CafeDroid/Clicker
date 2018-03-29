1c72a10d2c7b237c15539ca372df414ab8a9eef7
rachit <rachitbhutani1998@gmail.com>
2018-03-29 23:40:01 +0530

    test

--- app/src/main/java/com/cafedroid/android/clicker/FloatingViewService.java
+++ app/src/main/java/com/cafedroid/android/clicker/FloatingViewService.java
@@ -1,9 +1,17 @@
 package com.cafedroid.android.clicker;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
+import android.os.Environment;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -11,9 +19,25 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-public class FloatingViewService extends Service {
+import com.androidhiddencamera.CameraConfig;
+import com.androidhiddencamera.HiddenCameraService;
+import com.androidhiddencamera.config.CameraFacing;
+import com.androidhiddencamera.config.CameraImageFormat;
+import com.androidhiddencamera.config.CameraResolution;
+import com.androidhiddencamera.config.CameraRotation;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.security.auth.login.LoginException;
+
+import static android.content.ContentValues.TAG;
+
+public class FloatingViewService extends HiddenCameraService {
     private WindowManager mWindowManager;
     private View mFloatingView;
+    private CameraConfig mConfig;
 
     public FloatingViewService() {
     }
@@ -23,32 +47,44 @@ public class FloatingViewService extends Service {
         return null;
     }
 
+    @SuppressLint("InflateParams")
     @Override
     public void onCreate() {
         super.onCreate();
-        mFloatingView= LayoutInflater.from(this).inflate(R.layout.floating_button,null);
-        View button=mFloatingView.findViewById(R.id.image_action);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FloatingViewService.this, "Button is clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.floating_button, null);
+
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
 
-        params.gravity = Gravity.TOP | Gravity.START;
+        mConfig = new CameraConfig()
+                .getBuilder(getApplicationContext())
+                .setCameraFacing(CameraFacing.FRONT_FACING_CAMERA)
+                .setCameraResolution(CameraResolution.MEDIUM_RESOLUTION)
+                .setImageFormat(CameraImageFormat.FORMAT_JPEG)
+                .setImageRotation(CameraRotation.ROTATION_270)
+                .build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        startCamera(mConfig);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
         params.x = 0;
         params.y = 100;
 
-        mWindowManager=(WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         if (mWindowManager != null) {
-            mWindowManager.addView(mFloatingView,params);
+            mWindowManager.addView(mFloatingView, params);
         }
+
+        View button = mFloatingView.findViewById(R.id.collapse_view);
+
+
 //        int mWidth= (this.getResources().getDisplayMetrics().widthPixels)/2;
 //        int mHeight= (this.getResources().getDisplayMetrics().heightPixels)/2;
         button.setOnTouchListener(new View.OnTouchListener() {
@@ -56,16 +92,33 @@ public class FloatingViewService extends Service {
             private int initialY;
             private float initialTouchX;
             private float initialTouchY;
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+
                         //remember the initial position.
                         initialX = params.x;
                         initialY = params.y;
+
                         //get the touch location
                         initialTouchX = motionEvent.getRawX();
                         initialTouchY = motionEvent.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        int Xdiff = (int) (motionEvent.getRawX() - initialTouchX);
+                        int Ydiff = (int) (motionEvent.getRawY() - initialTouchY);
+
+
+                        //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
+                        //So that is click event.
+                        if (Xdiff < 10 && Ydiff < 10) {
+                                //When user clicks on the image view of the collapsed layout,
+                                //visibility of the collapsed layout will be changed to "View.GONE"
+                                //and expanded view will become visible.
+                            captureImage();
+                        }
                         return true;
                     case MotionEvent.ACTION_MOVE:
                         //Calculate the X and Y coordinates of the view.
@@ -81,9 +134,34 @@ public class FloatingViewService extends Service {
         });
     }
 
+    private void captureImage() {
+        //TODO: Capture image on click
+        takePicture();
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
     }
+
+    @Override
+    public void onImageCapture(@NonNull File imageFile) {
+        Log.e(TAG, "onImageCapture: "+imageFile.getAbsolutePath() );
+    }
+
+    @Override
+    public void onCameraError(int errorCode) {
+
+    }
 }
+
+//                    case MotionEvent.ACTION_DOWN:
+//                            //remember the initial position.
+//                            initialX = params.x;
+//                            initialY = params.y;
+//                            //get the touch location
+//                            initialTouchX = motionEvent.getRawX();
+//                            initialTouchY = motionEvent.getRawY();
+//                            return true;
--- app/src/main/java/com/cafedroid/android/clicker/MainActivity.java
+++ app/src/main/java/com/cafedroid/android/clicker/MainActivity.java
@@ -1,13 +1,74 @@
 package com.cafedroid.android.clicker;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2184;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check if the application has draw over other apps permission or not?
+        //This permission is by default available for API<23. But for API > 23
+        //you have to ask for the permission in runtime.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
+    }
+
+    private void initializeView() {
+        findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    startService(new Intent(MainActivity.this, FloatingViewService.class));
+                    finish();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        }else if (requestCode==101){
+            if (resultCode==RESULT_OK)
+                initializeView();
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
